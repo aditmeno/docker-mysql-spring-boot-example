8cad4e9a5c1608976dce7639f9f4fde7fc54e88b
Aditya Menon <adityamenon5@gmail.com>
2020-02-28 10:10:50 +0530

    Added descriptive comments

--- infrastructure/modules/so1-namespace/variable.tf
+++ infrastructure/modules/so1-namespace/variable.tf
@@ -1,17 +1,17 @@
 variable "prod_namespace" {
-  description = "The name for the so1-java app namespace"
+  description = "The name for the so1-java app production namespace"
   type        = string
   default     = "prod"
 }
 
 variable "dev_namespace" {
-  description = "The name for the so1-java app namespace"
+  description = "The name for the so1-java app development namespace"
   type        = string
   default     = "dev"
 }
 
 variable "qa_namespace" {
-  description = "The name for the so1-java app namespace"
+  description = "The name for the so1-java app QA namespace"
   type        = string
   default     = "qa"
 }
